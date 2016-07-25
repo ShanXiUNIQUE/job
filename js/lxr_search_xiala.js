e46a52f2df8ac7440fc5da41edf48b038809f209
sxueklxr <429115937@qq.com>
2016-07-25 08:39:40 +0800

    gengxin

--- js/lxr_search_xiala.js
+++ js/lxr_search_xiala.js
@@ -1,4 +1,6 @@
 $(function(){
+
+	// 下拉
 	$(".lxr_hastitle_right").click(function(){
 		$(".lxr_search_xiala>ul").fadeIn();
 
@@ -7,4 +9,11 @@ $(function(){
 			$(".lxr_search_xiala>ul").fadeOut();
 		})
 	})
+
+	// 选项卡
+	$(".lxr_title_center div").click(function(){
+		$(".lxr_title_center div").eq($(this).index()).addClass("lxr_change").siblings().removeClass("lxr_change");
+		$(".lxr_has_content ul").hide().eq($(this).index()).show();
+	})
+
 })

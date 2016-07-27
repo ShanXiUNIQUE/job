dd6a4433839119b6aaac1e12261b2a923a60efe7
sxueklxr <429115937@qq.com>
2016-07-27 20:11:32 +0800

    xiugai3

--- js/lxr_search_zhezhao.js
+++ js/lxr_search_zhezhao.js
@@ -16,17 +16,26 @@ $(function () {
 	$(".lxr_input").focus(function(){
 		if((!$(".lxr_input").val())||($(".lxr_input").val()=="Search"))
 			$(".lxr_input").val("");
+		$("#lxr_xx").css("display","block");
 	})
 	
 	$(".lxr_input").blur(function(){
 		if(!$(".lxr_input").val())
 			$(".lxr_input").val("Search");
+		$("#lxr_xx").css("display","none");
 	})
 
 	$(".lxr_input").keypress(function(e){
 		if (e.keyCode == 13) {
 			$(".lxr_zhezhao_min>form").submit();
 		};
+	})
+
+	$("#lxr_xx").click(function(e){
+		e.preventDefault();
+		$(".lxr_input").val("");
+		$(".lxr_input").blur();
+		$("#lxr_xx").css("display","none");
 	})
 	
 })
